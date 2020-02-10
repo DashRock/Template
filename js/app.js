60518e369e1d084e044c77ced3eea549cc08bfa3
DashRock <dash.lalonde@protonmail.com>
2020-02-10 13:01:41 -0800

    Updated Sidebar

--- js/app.js
+++ js/app.js
@@ -13,15 +13,17 @@ function togNav(){
     hamC.style.marginLeft = 0 + 'px';
     hamC.style.marginTop = 0 + 'px';
     hamC.style.width = 400 + 'px';
-  
-    
+    hamC.style.animation = 'entry 750ms ease-in';
     ele2.style.display = "flex";
     hamO.removeEventListener('click', togNav);
     hamO.addEventListener('click', togOn);
     function togOn(){
+        hamC.style.animation = 'exit 1000ms ease-out';
         ele1.style.display = "flex";
         ele2.style.display = "none";
-        hamC.style.display = "none";
+        
+        hamC.style.display = 'none';
+        
         hamO.addEventListener('click', togNav);
     }
 
